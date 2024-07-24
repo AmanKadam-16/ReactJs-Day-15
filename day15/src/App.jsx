547c3cd2aa99c3e71444f2bff730a087529f81c1
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2024-07-24 15:09:14 +0000

    useState and useEffect | Hooks

--- day15/src/App.jsx
+++ day15/src/App.jsx
@@ -1,44 +1,20 @@
-// import { Link, useNavigate } from "react-router-dom";
-
-
-// const App = () =>{
-// // useNavigate hook for routing through pages | components
-//   const navigate = useNavigate();
-
-//   function clickAbout(){
-//     navigate('/about')
-//   }
-
-//   return(
-//     <>
-//     <h2>Home</h2>
-//     <h3>This is home page.</h3>
-//     {/* <h2 onClick={clickAbout} style={{ cursor:'pointer' }} >about</h2> */}
-//     <Link to='/about' >About</Link>
-//     </>
-//   )
-// }
-
-// export default App;
-import './App.css'
-
 import { useState } from "react";
-const style = {color:'grey', fontWeight:'600'}  //object  | Internal CSS
+
 const App = () =>{
+// useState() | Hook
 
-  // let x = 1;
-  const [count, setNumber] = useState(0)  // 1,2,3,4
-
-  function increment(){
-    setNumber(count+1)
-  }
+const [name, setName]  = useState('abc')
 
   return(
-    <>
-    
-   <h1 className='heading1'>{count}</h1> 
 
-   <button onClick={increment}>increment</button>
+    <>
+    <button onClick={()=>setName('John')}>change state of name</button>
+    <h1>current state of name is {name}</h1>
+    {/* // e | e.target.value */}
+    {/* <input type="text" value={name} onChange={
+      (e)=>{setName(e.target.value)
+         console.log('current value',name)
+      }} /> */}
     </>
   )
 }
